5484b8d7525da389e74fa44efa646380d2bfa82f
Ace Habes <72156174+Ace-0101@users.noreply.github.com>
2021-02-01 17:05:46 -0800

    signup+login

--- express-project-starter-master/routes/users.js
+++ express-project-starter-master/routes/users.js
@@ -13,22 +13,39 @@ router.get('/signup', csrfProtection, (req, res) => {
 
 router.post('/signup', csrfProtection, asyncHandler(async(req, res) => {
     const { fullName, email, password, confirmPassword } = req.body
-    if (password === confirmPassword) {
-
-    }
-    res.redirect('/')
-
-
-
-
-
 
     const hashedPassword = await bcrypt.hash(password, 8);
+    await db.User.create({
+        fullName,
+        email,
+        password: hashedPassword,
+        confirmPassword,
+    });
 
 
+    loginUser(req, res, user);
+    res.redirect('/')
 }))
 
-/* GET users listing. */
+
+
+router.get('/login', csrfProtection, (req, res) => {
+    res.render('login', { csrfToken: req.csrfToken() })
+
+})
+
+
+router.post('/login', csrfProtection, asyncHandler(async(req, res) => {
+        const { email, password } = req.body
+        const user = await db.User.findOne({ where: { email } })
+        const matchingPassword = await bcrypt.compare(password, user.hashedPassword)
+
+        if (matchingPassword) {
+            loginUser(req, res, user)
+        }
+        res.redirect('/users')
+    }))
+    /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
