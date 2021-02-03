2ae7e5c3d06310ec2205b907169e9c0670ed5028
Christopher Kyle <ckyle6300@outlook.com>
2021-02-03 14:47:43 -0600

    sucessful seed

---
+++ db/seeders/20210203190315-productseeds.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert('Products', [
+      {
+        productName: "Test Product", description: "Test Product Description", defaultImg: "Photo"
+      }
+    ], {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
