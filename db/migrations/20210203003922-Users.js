ca71bc93d50d380d7c6057ad63a15efabebce5f8
Jamie Kichuk <jckichuk@gmail.com>
2021-02-02 18:44:52 -0600

    new migration

---
+++ db/migrations/20210203003922-Users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn('Users', 'biography', {
+        type: Sequelize.TEXT,
+        allowNull: true
+      })
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([queryInterface.removeColumn('Users', 'biography')])
+  }
+};
