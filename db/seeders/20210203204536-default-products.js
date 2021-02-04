9b31ed2fd5fd7646189db4321ea42f8f022b4a0a
Jamie Kichuk <jckichuk@gmail.com>
2021-02-04 15:28:24 -0600

    updated seeds

--- db/seeders/20210203204536-default-products.js
+++ db/seeders/20210203204536-default-products.js
@@ -1,3 +1,28 @@
+// 'use strict';
+
+// const faker = require('faker');
+// const { num_fake_products } = require('../../config/index').db
+
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     const defaultProducts = [];
+
+//     for (let i = 0; i < num_fake_products; i++) {
+//       defaultProducts.push({
+//         productName: faker.commerce.productName(),
+//         description: faker.commerce.productDescription(),
+//         defaultImg: faker.image.image()
+//       })
+//     }
+
+//     return queryInterface.bulkInsert('Products', defaultProducts, {})
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('Products', null, { truncate: true, cascade: true, restartIdentity: true });
+//   }
+// };
+
 'use strict';
 
 const faker = require('faker');
@@ -7,11 +32,34 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const defaultProducts = [];
 
+
     for (let i = 0; i < num_fake_products; i++) {
       defaultProducts.push({
         productName: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
-        defaultImg: faker.image.image()
+        defaultImg: faker.image.image(),
+        dayMade: 'Today'
+      })
+
+      defaultProducts.push({
+        productName: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        defaultImg: faker.image.image(),
+        dayMade: 'Yesterday'
+      })
+
+      defaultProducts.push({
+        productName: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        defaultImg: faker.image.image(),
+        dayMade: 'Last Week'
+      })
+
+      defaultProducts.push({
+        productName: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        defaultImg: faker.image.image(),
+        dayMade: 'Last Month'
       })
     }
 
