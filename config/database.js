e9aaa67d0177d35badd5234f702326802cf0e074
Ace Habes <72156174+Ace-0101@users.noreply.github.com>
2021-02-04 13:31:00 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Does sequelize just look for a databasae.js file?
 //  yes, as specified by sequelizerc
 
@@ -20,4 +21,24 @@ module.exports = {
         dialect: 'postgres',
         seederStorage: 'sequelize',
     }
+=======
+// Does sequelize just look for a databasae.js file?
+//  yes, as specified by sequelizerc
+
+const {
+    db: { username, password, database, host },
+} = require('./index');
+
+module.exports = {
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+        logging: false
+    },
+
+>>>>>>> Stashed changes
 };
