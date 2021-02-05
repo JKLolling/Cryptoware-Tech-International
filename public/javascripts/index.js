cf5c40a4a70ec8abe818b4d949ab5693da078fc8
Jamie Kichuk <jckichuk@gmail.com>
2021-02-04 21:31:38 -0600

    search bar returns json array of products containing the keyword substring

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,63 @@
 document.addEventListener('DOMContentLoaded', event => {
     const showMoreContentBtns = document.getElementsByClassName('productList_moreButton')
-
     for (let i = 0; i < showMoreContentBtns.length; i++) {
         const days = ['Today', 'Yesterday', 'Last Week', 'Last Month']
         showMoreContentBtns[i].addEventListener('click', async event => {
             try {
                 const content = await fetch(`/api/products/${days[i]}`)
-                // console.log(content)
                 const json = await content.json()
-                console.log(json)
+
+                const ul = event.target.previousElementSibling
+                for (let i = 0; i < json.length; i++) {
+                    const product = json[i]
+                    const newATag = document.createElement('a')
+                    const newli = document.createElement('li')
+                    const newImg = document.createElement('img')
+                    const divOuter = document.createElement('div')
+                    const nameDiv = document.createElement('div')
+                    const descriptionDiv = document.createElement('div')
+
+                    newATag.setAttribute('href', `/products/${product.id}`)
+                    newImg.setAttribute('src', `${product.defaultImg}`)
+                    newImg.setAttribute('alt', 'product image')
+                    nameDiv.setAttribute('class', 'productList_name')
+                    descriptionDiv.setAttribute('class', 'productList_description')
+
+                    nameDiv.innerText = product.productName
+                    descriptionDiv.innerText = product.description
+
+                    newATag.appendChild(newli)
+                    newli.appendChild(newImg)
+                    newli.appendChild(divOuter)
+                    divOuter.appendChild(nameDiv)
+                    divOuter.appendChild(descriptionDiv)
+
+                    ul.appendChild(newATag)
+                }
+                event.target.remove()
             } catch (err) {
                 console.error(error)
             }
         })
     }
+
+    const searchBar = document.getElementById('search_bar')
+    searchBar.addEventListener('keyup', async (event) => {
+
+        try {
+            const keyword = searchBar.value
+
+            if (keyword === '') {
+                return
+            }
+
+            const products = await fetch(`/api/products/keyword=${keyword}`)
+            const jsonProducts = await products.json()
+
+            console.log(jsonProducts)
+
+        } catch (error) {
+            console.log(error)
+        }
+    })
 })
--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,33 @@ const express = require('express');
 
 const { asyncHandler } = require('./utils')
 const { Product } = require('../db/models')
+const { Sequelize } = require('../db/models')
 const router = express.Router();
+const Op = Sequelize.Op;
 
 router.get('/products/:day(Today|Yesterday|Last%20Week|Last%20Month)', asyncHandler(async (req, res) => {
   const day = req.params.day
 
   const moreProducts = await Product.findAll({
     where: { dayMade: day },
-    limit: 20
+    limit: 35
   })
 
   res.json(moreProducts.slice(10))
+}))
+
+router.get('/products/keyword=:keyword', asyncHandler(async (req, res) => {
+  const keyword = req.params.keyword
+
+  const products = await Product.findAll({
+    where: {
+      productName: {
+        [Op.substring]: keyword
+      }
+    }
+  })
+
+  res.json(products)
 }))
 
 module.exports = router;
